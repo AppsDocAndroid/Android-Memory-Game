9cd32cf078eae613fde6b8656a406ed2df80d440
Gert Rietveld <muggezifter@gmail.com>
2012-01-23 16:31:22 +0100

    Some cleaning up

--- src/com/gertrietveld/memorygame/Settings.java
+++ src/com/gertrietveld/memorygame/Settings.java
@@ -3,13 +3,11 @@ package com.gertrietveld.memorygame;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.Toast;
-
-import android.content.SharedPreferences;
 
 
 public class Settings extends Activity {
